ca8db1288a11aa4a90cd5f713b2e8335507dc11f
Daniel Butterfield <dan.butterfield@hotmail.co.uk>
2021-12-03 18:27:36 +0000

    Add imports to server.js

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 import express from 'express'
+import { dayOnePartOne } from './2021/index.js'
 
 const app = express()
 const port = 8080
 
 app.listen(port, () => {
-  console.log('app started')
+  console.log('Advent of Code')
+
+  const dayOnePartOneValue = dayOnePartOne()
+
+  console.log(`D1 P1: ${dayOnePartOneValue}`)
 })
 
 app.get('/', (req, res) => {
