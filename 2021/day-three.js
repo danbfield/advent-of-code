7880f842bcf4b06a58fdb93617ae0b132b0691f4
Daniel Butterfield <dan.butterfield@hotmail.co.uk>
2021-12-05 05:41:06 +0000

    Refactor bit position function(s) into one function

--- 2021/day-three.js
+++ 2021/day-three.js
@@ -4,7 +4,7 @@ const input = parseInputAsStrings('2021/inputs/day-three.txt')
 
 const numberOfCharacters = 12
 
-const getMostCommonBitInPosition = (diagnostics, position) => {
+const getBitInPosition = (diagnostics, position, range) => {
   let bit0 = 0
   let bit1 = 0
 
@@ -14,30 +14,14 @@ const getMostCommonBitInPosition = (diagnostics, position) => {
     if (character === '1') bit1++
   }
 
-  return bit0 > bit1 ? '0' : '1'
-}
-
-const getLeastCommonBitInPosition = (diagnostics, position) => {
-  let bit0 = 0
-  let bit1 = 0
-
-  for (let i = 0; i < diagnostics.length; i++) {
-    const character = diagnostics[i].charAt(position)
-    if (character === '0') bit0++
-    if (character === '1') bit1++
-  }
-
-  return bit0 < bit1 ? '0' : '1'
+  return range === 'max' ? (bit0 > bit1 ? '0' : '1') : bit0 < bit1 ? '0' : '1'
 }
 
 const getUnitRate = (diagnostics, range) => {
   let values = []
 
   for (let position = 0; position < numberOfCharacters; position++) {
-    const value =
-      range === 'max'
-        ? getLeastCommonBitInPosition(diagnostics, position)
-        : getMostCommonBitInPosition(diagnostics, position)
+    const value = getBitInPosition(diagnostics, position, range)
 
     values.push(value)
   }
@@ -50,6 +34,7 @@ const gammaRate = getUnitRate(input, 'max')
 const epsilonRate = getUnitRate(input, 'min')
 
 const dayThreePartOne = gammaRate * epsilonRate
+
 const dayThreePartTwo = '🚧 WIP 🚧'
 
 export { dayThreePartOne, dayThreePartTwo }
