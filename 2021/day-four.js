9f3398a921e4c47918db86eeee95ca72907880c8
Daniel Butterfield <dan.butterfield@hotmail.co.uk>
2021-12-06 16:18:27 +0000

    Add formatInput to split our input

--- 2021/day-four.js
+++ 2021/day-four.js
@@ -2,6 +2,35 @@ import { parseInputByCarriageReturn } from './inputs/helper.js'
 
 const input = parseInputByCarriageReturn('2021/inputs/test.txt')
 
+const formatInput = (input) => {
+  const arrayOfDrawnNumbers = input[0].split([','])
+
+  let currentBoard = []
+  let boards = []
+
+  // Ignore the first line, which is our drawn numbers
+  for (let i = 1; i < input.length; i++) {
+    const currentLine = input[i].trim()
+
+    if (currentLine === '' && currentBoard.length) {
+      boards.push(currentBoard)
+      currentBoard = []
+    }
+
+    if (currentLine !== '') {
+      const formattedString = currentLine.replace(/ +/g, ' ')
+      // currentBoard.push(formattedString.split(' '))
+      // Should I split the string into an array of 5 values? Hmm...
+      currentBoard.push(formattedString)
+    }
+  }
+
+  return [arrayOfDrawnNumbers, boards]
+}
+
+const [drawnNumbers, boards] = formatInput(input)
+
+console.log({ drawnNumbers, boards })
 const solve = () => {
   // ...
   return 'something'
