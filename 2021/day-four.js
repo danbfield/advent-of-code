1a872e21c5731df337563b16ebe898e8e78b2f0b
Daniel Butterfield <dan.butterfield@hotmail.co.uk>
2021-12-06 16:26:01 +0000

    Add explanation for formatInput

--- 2021/day-four.js
+++ 2021/day-four.js
@@ -2,6 +2,35 @@ import { parseInputByCarriageReturn } from './inputs/helper.js'
 
 const input = parseInputByCarriageReturn('2021/inputs/test.txt')
 
+/**
+ * Formats the input of parseInputByCarriageReturn which does two things.
+ * Firstly, it takes the first line, and splits it by "," which creates
+ * our bingo drawn numbers array. Then, it loops over the remaining lines
+ * and creates the bingo boards. I did cheat a bit, on the final line
+ * of the text file... just add a carriage return. Otherwise the final
+ * board won't be added to the array. I couldn't be bothered adding a few
+ * extra lines of code when I could just add a return.
+ *
+ * The function returns an object which includes the drawn numbers, and
+ * an array of boards. Each line of a board is a string, however it could
+ * be split into it's own array using split(' ').
+ *
+ * { drawnNumbers:
+ *    [ '7',  '4',  '9',  '5',  '11', ],
+ *    boards: [
+ *      [
+ *        '22 13 17 11 0',
+ *        '8 2 23 4 24',
+ *        '21 9 14 16 7',
+ *        '6 10 3 18 5',
+ *        '1 12 20 15 19'
+ *      ]
+ *   ]
+ * }
+ *
+ * @param string[] input
+ * @returns object
+ */
 const formatInput = (input) => {
   const arrayOfDrawnNumbers = input[0].split([','])
 
@@ -31,6 +60,7 @@ const formatInput = (input) => {
 const [drawnNumbers, boards] = formatInput(input)
 
 console.log({ drawnNumbers, boards })
+
 const solve = () => {
   // ...
   return 'something'
