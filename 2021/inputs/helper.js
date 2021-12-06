b922507050d041d724ef606b1a014defac60a8bc
Daniel Butterfield <dan.butterfield@hotmail.co.uk>
2021-12-06 16:14:28 +0000

    Add helper function for '/r'

--- 2021/inputs/helper.js
+++ 2021/inputs/helper.js
@@ -3,5 +3,8 @@ import fs from 'fs'
 export const parseInputAsStrings = (file) =>
   fs.readFileSync(file, 'utf-8').trim().split('\n')
 
+export const parseInputByCarriageReturn = (file) =>
+  fs.readFileSync(file, 'utf-8').split('\r')
+
 export const parseInputAsNumbers = (file) =>
   parseInputAsStrings(file).map((item) => parseInt(item))
