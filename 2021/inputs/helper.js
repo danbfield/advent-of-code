bb2c151ee307dcf0bb0cf8c8ae5b46a70780d2f4
Daniel Butterfield <dan.butterfield@hotmail.co.uk>
2021-12-07 10:52:57 +0000

    Add string replace on "\r"

--- 2021/inputs/helper.js
+++ 2021/inputs/helper.js
@@ -1,7 +1,11 @@
 import fs from 'fs'
 
 export const parseInputAsStrings = (file) =>
-  fs.readFileSync(file, 'utf-8').trim().split('\n')
+  fs
+    .readFileSync(file, 'utf-8')
+    .trim()
+    .split('\n')
+    .map((item) => item.replace(/[\n\r]+/g, ''))
 
 export const parseInputByCarriageReturn = (file) =>
   fs.readFileSync(file, 'utf-8').split('\r')
