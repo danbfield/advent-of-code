0c9ff901d245a2c0e7bac5085cbb1dd2ed93eada
Daniel Butterfield <dan.butterfield@hotmail.co.uk>
2021-12-07 09:46:07 +0000

    Add explanations for day four

--- 2021/day-four.js
+++ 2021/day-four.js
@@ -19,17 +19,17 @@ const input = parseInputByCarriageReturn('2021/inputs/day-four.txt')
  *    [ '7',  '4',  '9',  '5',  '11', ],
  *    boards: [
  *      [
- *        '22 13 17 11 0',
- *        '8 2 23 4 24',
- *        '21 9 14 16 7',
- *        '6 10 3 18 5',
- *        '1 12 20 15 19'
+ *        [22, 13, 17, 11, 0],
+ *        [8, 2, 23, 4, 24],
+ *        [21, 9, 14, 16, 7],
+ *        [6, 10, 3, 18, 5],
+ *        [1, 12, 20, 15, 19]
  *      ]
  *   ]
  * }
  *
  * @param string[] input
- * @returns object
+ * @returns array
  */
 const formatInput = (input) => {
   const arrayOfDrawnNumbers = input[0]
@@ -63,19 +63,39 @@ const formatInput = (input) => {
 
 const [numbers, boards] = formatInput(input)
 
-const getWinningResult = (board, calledNumbers) => {
-  const flatBoard = board.flat()
-  const lastNumber = parseInt(calledNumbers[calledNumbers.length - 1])
-
-  const excludedNumbers = flatBoard.filter(
-    (boardNumbers) => !calledNumbers.includes(boardNumbers)
-  )
+/**
+ * This function calculates the result of the winning board. Start
+ * by finding the sum of all unmarked numbers on that board; in this
+ * case, the sum is 188. Then, multiply that sum by the number that
+ * was just called when the board won, 24, to get the final score,
+ * 188 * 24 = 4512.
+ *
+ * @param array[int] numbers
+ * @param array[array[int]] board
+ * @returns int
+ */
+const getWinningResult = (numbers, board) => {
+  const excludedNumbers = board
+    .flat()
+    .filter((boardNumbers) => !numbers.includes(boardNumbers))
 
   const summedExcludedNumbers = excludedNumbers.reduce((a, b) => a + b, 0)
 
-  return summedExcludedNumbers * lastNumber
+  return summedExcludedNumbers * numbers[numbers.length - 1]
 }
 
+/**
+ * This function checks if this board was in fact a winning board,
+ * based on the current numbers drawn in the game of bingo. The
+ * board itself is a 5x5 grid of numbers, and we cross check this,
+ * horizontally and vertically to see if each called bingo number
+ * is included within that row or column. Assuming either of the
+ * row or column wins bingo, return the board back.
+ *
+ * @param array[int] numbers
+ * @param array[array[int]] board
+ * @returns array[array[int]] board
+ */
 const getWinningBoard = (numbers, board) => {
   for (let x = 0; x < board.length; x++) {
     // We need this to get the columns, not the rows due to how we've
@@ -91,6 +111,22 @@ const getWinningBoard = (numbers, board) => {
   }
 }
 
+/**
+ * This function loops over each number (replicating as it's drawn)
+ * and checks against each board to see if the board meets the win
+ * condition of getting 5 marked numbers in a row, either vertically
+ * or horizontally. Every time we get a winning board, we add that
+ * board to an array of winners, and keep going until we get the
+ * last winner. Every time we get a winner, we also remove that board
+ * from the potential boards, slowly but surely getting less and less
+ * boards to play with.
+ *
+ * Eventually, there will be 1 board left (which solves part B).
+ *
+ * @param array[int] bingoNumbers
+ * @param array[array[int]] bingoBoards
+ * @returns
+ */
 const playBingo = (bingoNumbers, bingoBoards) => {
   const numbers = []
   const winners = []
@@ -108,16 +144,18 @@ const playBingo = (bingoNumbers, bingoBoards) => {
       if (winningBoard) {
         winners.push(winningBoard)
 
-        // We only care about the first winner here
+        // We only care about the first winner here, and we need to calculate
+        // the winning result immediately otherwise the array of drawn numbers
+        // would be different than when it won.
         if (winners.length === 1) {
-          partAResult = getWinningResult(winners[0], numbers)
+          partAResult = getWinningResult(numbers, winners[0])
         }
 
         if (bingoBoards.length > 1) {
           // Remove the winning board from array of potential boards
           bingoBoards = bingoBoards.filter((board) => board !== winningBoard)
         } else {
-          partBResult = getWinningResult(winners[winners.length - 1], numbers)
+          partBResult = getWinningResult(numbers, winners[winners.length - 1])
           return [partAResult, partBResult]
         }
       }
