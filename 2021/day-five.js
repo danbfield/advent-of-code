2f6542b59feff77ac408736284970dcde99837f4
Daniel Butterfield <dan.butterfield@hotmail.co.uk>
2021-12-07 12:36:55 +0000

    Add formatCoordinates function to parse initial input

--- 2021/day-five.js
+++ 2021/day-five.js
@@ -6,4 +6,29 @@ const dayFivePartOne = null
 
 const dayFivePartTwo = null
 
+/**
+ * Builds an array of inputs into a more usable format. Each coordinate
+ * is an array of arrays, for the before and after.
+ *
+ * [
+ *   [ [ '0,9' ], [ '5,9' ] ],
+ *   [ [ '8,0' ], [ '0,8' ] ]
+ * ]
+ *
+ * @param string[] input
+ * @returns array
+ */
+const formatCoordinates = (input) =>
+  input.map((line) => line.split('->').map((coordinate) => [coordinate.trim()]))
+
+const convertCoordinatesIntoDiagram = (coordinates) => {
+  console.log(coordinates)
+  const diagram = []
+
+  return diagram
+}
+
+const coordinates = formatCoordinates(input)
+const diagram = convertCoordinatesIntoDiagram(coordinates)
+
 export { dayFivePartOne, dayFivePartTwo }
