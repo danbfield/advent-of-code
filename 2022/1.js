168c9dbb390840493706559fbcc0ea498460dcfc
Dan Butterfield <danrbutterfield@gmail.com>
2022-12-01 18:05:25 +0000

    Reduce line count with export

--- 2022/1.js
+++ 2022/1.js
@@ -12,8 +12,5 @@ const sum = (partialSum, a) => partialSum + a
 
 const output = input.map((value) => value.reduce(sum, 0)).sort((a, b) => b - a)
 
-const d1p1 = output[0]
-
-const d1p2 = output.slice(0, 3).reduce(sum, 0)
-
-export { d1p1, d1p2 }
+export const d1p1 = output[0]
+export const d1p2 = output.slice(0, 3).reduce(sum, 0)
