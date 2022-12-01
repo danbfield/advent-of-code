bb0e39890757fbd88254f6d036884b0a9da588f5
Dan Butterfield <danrbutterfield@gmail.com>
2022-12-01 12:48:01 +0000

    Solve day 1 part 2

--- 2022/1.js
+++ 2022/1.js
@@ -8,7 +8,11 @@ const array = []
 for (let i = 0; i < input.length; i++) {
   if (input[i] !== '') {
     total = total + parseInt(input[i])
-  } else {
+  }
+
+  // Splits between "elves" and handles
+  // the last iteration
+  if (input[i] === '' || i == input.length - 1) {
     array.push(total)
     total = 0
   }
@@ -16,6 +20,11 @@ for (let i = 0; i < input.length; i++) {
 
 const sorted = array.sort((a, b) => b - a)
 
-const day1 = sorted[0]
+const d1p1 = sorted[0]
 
-export { day1 }
+const d1p2 = [sorted[0], sorted[1], sorted[2]].reduce(
+  (partialSum, a) => partialSum + a,
+  0
+)
+
+export { d1p1, d1p2 }
--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 import express from 'express'
-import { day1 } from './2022/1.js'
+import { d1p1, d1p2 } from './2022/1.js'
 
 const app = express()
 const port = 8080
 
 app.listen(port, () => {
   console.log('🎄 Advent of Code 🎅')
-  console.log({ 'day1.0': day1, 'day1.5': 'n/a' })
+  console.log({ 'day1.0': d1p1, 'day1.5': d1p2 })
 })
 
 app.get('/', (req, res) => {
