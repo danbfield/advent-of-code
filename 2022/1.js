5eb55d40f64f8477d0cb90934b59f3d46e3dd5b4
Dan Butterfield <danrbutterfield@gmail.com>
2022-12-01 12:40:09 +0000

    Tidy up a lil bit

--- 2022/1.js
+++ 2022/1.js
@@ -2,10 +2,7 @@ import { parseInputAsStrings } from '../inputs/helper.js'
 
 const input = parseInputAsStrings('inputs/2022/1.txt')
 
-const dayOne = ''
-
 let total = 0
-
 const array = []
 
 for (let i = 0; i < input.length; i++) {
@@ -19,6 +16,6 @@ for (let i = 0; i < input.length; i++) {
 
 const sorted = array.sort((a, b) => b - a)
 
-console.log(sorted[0])
+const day1 = sorted[0]
 
-export { dayOne }
+export { day1 }
--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 import express from 'express'
-import { dayOne } from './2022/1.js'
+import { day1 } from './2022/1.js'
 
 const app = express()
 const port = 8080
 
 app.listen(port, () => {
   console.log('🎄 Advent of Code 🎅')
+  console.log({ 'day1.0': day1, 'day1.5': 'n/a' })
 })
 
 app.get('/', (req, res) => {
