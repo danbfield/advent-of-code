a55c60fae70b6a2ccaab22598f816fb5490b4a15
Dan Butterfield <danrbutterfield@gmail.com>
2022-12-01 18:04:40 +0000

    Replace reduce anon func with callback sum

--- 2022/1.js
+++ 2022/1.js
@@ -8,12 +8,12 @@ const input = deepConvertStringToInt(
   splitOnSeparator(parseInputAsStrings('inputs/2022/1.txt'))
 )
 
-const output = input
-  .map((value) => value.reduce((partialSum, a) => partialSum + a, 0))
-  .sort((a, b) => b - a)
+const sum = (partialSum, a) => partialSum + a
+
+const output = input.map((value) => value.reduce(sum, 0)).sort((a, b) => b - a)
 
 const d1p1 = output[0]
 
-const d1p2 = output.slice(0, 3).reduce((partialSum, a) => partialSum + a, 0)
+const d1p2 = output.slice(0, 3).reduce(sum, 0)
 
 export { d1p1, d1p2 }
