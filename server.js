c01963d9fe142fd76fdc8522cda6d603dba24334
Dan Butterfield <danrbutterfield@gmail.com>
2022-12-01 11:38:40 +0000

    Remove 2021 from server.js

--- server.js
+++ server.js
@@ -1,33 +1,10 @@
 import express from 'express'
-import {
-  dayOnePartOne,
-  dayOnePartTwo,
-  dayTwoPartOne,
-  dayTwoPartTwo,
-  dayThreePartOne,
-  dayThreePartTwo,
-  dayFourPartOne,
-  dayFourPartTwo,
-  dayFivePartOne,
-  dayFivePartTwo,
-} from './2021/index.js'
 
 const app = express()
 const port = 8080
 
 app.listen(port, () => {
   console.log('🎄 Advent of Code 🎅')
-
-  console.log(`D1 P1: ${dayOnePartOne}`)
-  console.log(`D1 P2: ${dayOnePartTwo}`)
-  console.log(`D2 P1: ${dayTwoPartOne}`)
-  console.log(`D2 P2: ${dayTwoPartTwo}`)
-  console.log(`D3 P1: ${dayThreePartOne}`)
-  console.log(`D3 P2: ${dayThreePartTwo}`)
-  console.log(`D4 P1: ${dayFourPartOne}`)
-  console.log(`D4 P2: ${dayFourPartTwo}`)
-  console.log(`D5 P1: ${dayFivePartOne}`)
-  console.log(`D5 P2: ${dayFivePartTwo}`)
 })
 
 app.get('/', (req, res) => {
