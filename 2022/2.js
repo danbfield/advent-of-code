276871764bba32ef639885c52542c3e09f0f34ec
Dan Butterfield <danrbutterfield@gmail.com>
2022-12-02 16:05:56 +0000

    Add d2p1

---
+++ 2022/2.js
@@ -0,0 +1,58 @@
+import { parseInputAsStrings } from '../inputs/helper.js'
+
+const input = parseInputAsStrings('inputs/2022/2.txt').map((element) =>
+  element.split(' ')
+)
+
+const scores = {
+  A: 1,
+  X: 1,
+  B: 2,
+  Y: 2,
+  C: 3,
+  Z: 3,
+}
+
+const DRAW = 3
+const WIN = 6
+const LOSS = 0
+
+const scoreEarnedThisRound = (opponentMove, playerMove) => {
+  let scoreThisRound = 0
+
+  switch (Math.abs(opponentMove - playerMove)) {
+    case 0:
+      scoreThisRound += DRAW
+      break
+    case 1:
+      if (opponentMove > playerMove) {
+        scoreThisRound += LOSS
+      } else {
+        scoreThisRound += WIN
+      }
+      break
+    default:
+      if (opponentMove < playerMove) {
+        scoreThisRound += LOSS
+      } else {
+        scoreThisRound += WIN
+      }
+  }
+
+  scoreThisRound += playerMove
+
+  return scoreThisRound
+}
+
+let total = 0
+
+for (let i = 0; i < input.length; i++) {
+  const [opponentMove, playerMove] = input[i]
+
+  const opponentMoveScore = scores[opponentMove]
+  const playerMoveScore = scores[playerMove]
+
+  total += scoreEarnedThisRound(opponentMoveScore, playerMoveScore)
+}
+
+export const d2p1 = total
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import { d1p1, d1p2 } from './2022/1.js'
+import { d2p1 } from './2022/2.js'
 
 const app = express()
 const port = 8080
@@ -7,6 +8,7 @@ const port = 8080
 app.listen(port, () => {
   console.log('🎄 Advent of Code 🎅')
   console.log({ 'day1.0': d1p1, 'day1.5': d1p2 })
+  console.log({ 'day2.0': d2p1, 'day2.5': 'n/a' })
 })
 
 app.get('/', (req, res) => {
