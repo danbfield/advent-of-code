09e21ced5ca94e31e585738655f706d44338374d
Dan Butterfield <danrbutterfield@gmail.com>
2022-12-02 21:37:47 +0000

    Add part 2

--- 2022/2.js
+++ 2022/2.js
@@ -3,21 +3,24 @@ import { parseInputAsStrings } from '../inputs/helper.js'
 const input = parseInputAsStrings('inputs/2022/2.txt')
 
 const score = {
-  'A X': 3 + 1,
-  'A Y': 6 + 2,
-  'A Z': 0 + 3,
-  'B X': 0 + 1,
-  'B Y': 3 + 2,
-  'B Z': 6 + 3,
-  'C X': 6 + 1,
-  'C Y': 0 + 2,
-  'C Z': 3 + 3,
+  'A X': [3 + 1, 0 + 3],
+  'A Y': [6 + 2, 3 + 1],
+  'A Z': [0 + 3, 6 + 2],
+  'B X': [0 + 1, 0 + 1],
+  'B Y': [3 + 2, 3 + 2],
+  'B Z': [6 + 3, 6 + 3],
+  'C X': [6 + 1, 0 + 2],
+  'C Y': [0 + 2, 3 + 3],
+  'C Z': [3 + 3, 6 + 1],
 }
 
-let total = 0
+let p1 = 0
+let p2 = 0
 
 input.forEach((element) => {
-  total += score[element]
+  p1 += score[element][0]
+  p2 += score[element][1]
 })
 
-export const d2p1 = total
+export const d2p1 = p1
+export const d2p2 = p2
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import { d1p1, d1p2 } from './2022/1.js'
-import { d2p1 } from './2022/2.js'
+import { d2p1, d2p2 } from './2022/2.js'
 
 const app = express()
 const port = 8080
@@ -8,7 +8,7 @@ const port = 8080
 app.listen(port, () => {
   console.log('🎄 Advent of Code 🎅')
   console.log({ 'day1.0': d1p1, 'day1.5': d1p2 })
-  console.log({ 'day2.0': d2p1, 'day2.5': 'n/a' })
+  console.log({ 'day2.0': d2p1, 'day2.5': d2p2 })
 })
 
 app.get('/', (req, res) => {
