c3b7992e8fb172b64ee1ecaf2c7712f7408d5f51
Dan Butterfield <danrbutterfield@gmail.com>
2022-12-05 17:30:47 +0000

    Add part 4 boilerplate

---
+++ 2022/4.js
@@ -0,0 +1,8 @@
+import { parseInputAsStrings } from '../inputs/helper.js'
+
+const input = parseInputAsStrings('inputs/2022/4.txt')
+
+console.log(input)
+
+export const d4p1 = 'n/a'
+export const d4p2 = 'n/a'
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from 'express'
 import { d1p1, d1p2 } from './2022/1.js'
 import { d2p1, d2p2 } from './2022/2.js'
 import { d3p1, d3p2 } from './2022/3.js'
+import { d4p1, d4p2 } from './2022/4.js'
 
 const app = express()
 const port = 8080
@@ -11,6 +12,7 @@ app.listen(port, () => {
   console.log({ 'day1.0': d1p1, 'day1.5': d1p2 })
   console.log({ 'day2.0': d2p1, 'day2.5': d2p2 })
   console.log({ 'day3.0': d3p1, 'day3.5': d3p2 })
+  console.log({ 'day4.0': d4p1, 'day4.5': d4p2 })
 })
 
 app.get('/', (req, res) => {
