415754b8d205adbd140824047c03100b8128a3c8
Dan Butterfield <danrbutterfield@gmail.com>
2022-12-05 14:23:40 +0000

    Refactor day3 part 1

--- 2022/3.js
+++ 2022/3.js
@@ -1,60 +1,12 @@
 import { parseInputAsStrings } from '../inputs/helper.js'
 
-const values = [
-  'a',
-  'b',
-  'c',
-  'd',
-  'e',
-  'f',
-  'g',
-  'h',
-  'i',
-  'j',
-  'k',
-  'l',
-  'm',
-  'n',
-  'o',
-  'p',
-  'q',
-  'r',
-  's',
-  't',
-  'u',
-  'v',
-  'w',
-  'x',
-  'y',
-  'z',
-  'A',
-  'B',
-  'C',
-  'D',
-  'E',
-  'F',
-  'G',
-  'H',
-  'I',
-  'J',
-  'K',
-  'L',
-  'M',
-  'N',
-  'O',
-  'P',
-  'Q',
-  'R',
-  'S',
-  'T',
-  'U',
-  'V',
-  'W',
-  'X',
-]
+const lowerCase = 'abcdefghijklmnopqrstuvwxyz'.split('')
+const upperCase = lowerCase.map((letter) => letter.toUpperCase())
+const values = [...lowerCase, ...upperCase]
+
 const input = parseInputAsStrings('inputs/2022/3.txt')
 
-const commonItem = (a, b) => {
+const commonItems = (a, b) => {
   let duplicateCharacter = ''
 
   for (let i = 0; i < a.length; i++) {
@@ -68,24 +20,19 @@ const commonItem = (a, b) => {
   return duplicateCharacter
 }
 
-const value = []
+const items = []
 
 input.forEach((rucksack) => {
-  console.log(rucksack)
-
   const compartmentOne = rucksack.substring(0, rucksack.length / 2)
   const compartmentTwo = rucksack.substring(
     rucksack.length / 2,
     rucksack.length
   )
 
-  value.push(
-    values.findIndex(
-      (item) => item === commonItem(compartmentOne, compartmentTwo)
-    ) + 1
-  )
+  const commonItem = commonItems(compartmentOne, compartmentTwo)
+  const item = values.findIndex((item) => item === commonItem) + 1
+
+  items.push(item)
 })
 
-console.log(value)
-
-export const d3p1 = 'n/a'
+export const d3p1 = items.reduce((partialSum, a) => partialSum + a, 0)
