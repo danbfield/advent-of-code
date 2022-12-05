0c76c530ebd89a3c8323038c744d270f4ab87b7d
Dan Butterfield <danrbutterfield@gmail.com>
2022-12-05 14:00:55 +0000

    Add day3 boilerplate

---
+++ 2022/3.js
@@ -0,0 +1,5 @@
+import { parseInputAsStrings } from '../inputs/helper.js'
+
+const input = parseInputAsStrings('inputs/2022/3.txt')
+
+export const d3p1 = 'n/a'
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import { d1p1, d1p2 } from './2022/1.js'
 import { d2p1, d2p2 } from './2022/2.js'
+import { d3p1 } from './2022/3.js'
 
 const app = express()
 const port = 8080
@@ -9,6 +10,7 @@ app.listen(port, () => {
   console.log('🎄 Advent of Code 🎅')
   console.log({ 'day1.0': d1p1, 'day1.5': d1p2 })
   console.log({ 'day2.0': d2p1, 'day2.5': d2p2 })
+  console.log({ 'day3.0': d3p1, 'day3.5': 'n/a' })
 })
 
 app.get('/', (req, res) => {
