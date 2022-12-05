9fd7c38e311be7e851ac4d272c71b8807d958a42
Dan Butterfield <danrbutterfield@gmail.com>
2022-12-05 18:08:42 +0000

    Add section visualisation from example

--- 2022/4.js
+++ 2022/4.js
@@ -2,7 +2,53 @@ import { parseInputAsStrings } from '../inputs/helper.js'
 
 const input = parseInputAsStrings('inputs/2022/4.txt')
 
-console.log(input)
+String.prototype.replaceAt = function (index, replacement) {
+  return (
+    this.substring(0, index) +
+    replacement +
+    this.substring(index + replacement.length)
+  )
+}
+
+const getAllNumbersBetween = (beginning, end) => {
+  const numbers = []
+
+  for (let i = beginning; i <= end; i++) {
+    numbers.push(i)
+  }
+
+  return numbers
+}
+
+const getSections = (assignment) => {
+  let section = '........'
+
+  const [beginning, end] = assignment.split('-')
+
+  const assignedSections = getAllNumbersBetween(
+    parseInt(beginning),
+    parseInt(end)
+  )
+
+  assignedSections.forEach((assignedSection) => {
+    section = section.replaceAt(assignedSection - 1, assignedSection.toString())
+  })
+
+  return section
+}
+
+let count = 0
+
+for (let i = 0; i < input.length; i++) {
+  const [first, second] = input[i].split(',')
+
+  const firstSection = getSections(first)
+  const secondSection = getSections(second)
+
+  console.log({ firstSection, secondSection })
+}
+
+console.log(count)
 
 export const d4p1 = 'n/a'
 export const d4p2 = 'n/a'
