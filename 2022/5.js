fdf1800985fcaa242f53eae5a18d5ef8d314c476
Dan Butterfield <danrbutterfield@gmail.com>
2022-12-06 12:07:25 +0000

    Add flipRowToStack method

--- 2022/5.js
+++ 2022/5.js
@@ -6,12 +6,17 @@ console.log(input)
 
 const [cratesStr, instructionsStr] = input.split('\n\n')
 
+const flipRowToStack = (arr) =>
+  arr[0].map((_, i) => arr.map((row) => row[i]).reverse())
+
 const rowsStr = cratesStr.split('\n').slice(0, -1)
 const rowsArr = rowsStr.map((row) =>
   [...row.matchAll(/[A-Z]|    ?/g)].map((match) => match[0])
 )
 
-console.log(rowsArr)
+const stacks = flipRowToStack(rowsArr)
+
+console.log(stacks)
 
 export let d5p1 = 0
 export let d5p2 = 0
