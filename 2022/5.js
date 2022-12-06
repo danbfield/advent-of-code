7b150a5e1f279a0fe9d357469fa1c665e7e5eda4
Dan Butterfield <danrbutterfield@gmail.com>
2022-12-06 12:02:03 +0000

    Convert rows string to arr

--- 2022/5.js
+++ 2022/5.js
@@ -4,5 +4,14 @@ const input = simpleReadAsString('inputs/2022/5.txt')
 
 console.log(input)
 
+const [cratesStr, instructionsStr] = input.split('\n\n')
+
+const rowsStr = cratesStr.split('\n').slice(0, -1)
+const rowsArr = rowsStr.map((row) =>
+  [...row.matchAll(/[A-Z]|    ?/g)].map((match) => match[0])
+)
+
+console.log(rowsArr)
+
 export let d5p1 = 0
 export let d5p2 = 0
