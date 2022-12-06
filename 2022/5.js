1612620863a231cdda19fae4b43ab5f656e05746
Dan Butterfield <danrbutterfield@gmail.com>
2022-12-06 11:13:17 +0000

    Add day 5 boilerplate

---
+++ 2022/5.js
@@ -0,0 +1,6 @@
+import { parseInputAsStrings } from '../inputs/helper.js'
+
+const input = parseInputAsStrings('inputs/2022/5.txt')
+
+export let d5p1 = 0
+export let d5p2 = 0
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import { d1p1, d1p2 } from './2022/1.js'
 import { d2p1, d2p2 } from './2022/2.js'
 import { d3p1, d3p2 } from './2022/3.js'
 import { d4p1, d4p2 } from './2022/4.js'
+import { d5p1, d5p2 } from './2022/5.js'
 
 const app = express()
 const port = 8080
@@ -13,6 +14,7 @@ app.listen(port, () => {
   console.log({ 'day2.0': d2p1, 'day2.5': d2p2 })
   console.log({ 'day3.0': d3p1, 'day3.5': d3p2 })
   console.log({ 'day4.0': d4p1, 'day4.5': d4p2 })
+  console.log({ 'day5.0': d5p1, 'day5.5': d5p2 })
 })
 
 app.get('/', (req, res) => {
