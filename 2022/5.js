c44833eda9eb3caf16405bee20bfc04fc89d1b60
Dan Butterfield <danrbutterfield@gmail.com>
2022-12-07 09:13:04 +0000

    Refactor day 5 part 2 to use 1 for loop

--- 2022/5.js
+++ 2022/5.js
@@ -14,8 +14,8 @@ const rowsArr = rowsStr.map((row) =>
   [...row.matchAll(/[A-Z]|    ?/g)].map((match) => match[0])
 )
 
-export let d5p1 = flipRowToStack(rowsArr)
-export let d5p2 = flipRowToStack(rowsArr)
+export let stack1 = flipRowToStack(rowsArr)
+export let stack2 = flipRowToStack(rowsArr)
 
 const steps = instructionsStr
   .split('\n')
@@ -24,15 +24,13 @@ const steps = instructionsStr
 
 for (const step of steps) {
   const [number, from, to] = step
-  const crates = d5p1[from - 1].splice(-number).reverse()
-  d5p1[to - 1].push(...crates)
+
+  const crates1 = stack1[from - 1].splice(-number).reverse()
+  const crates2 = stack2[from - 1].splice(-number)
+
+  stack1[to - 1].push(...crates1)
+  stack2[to - 1].push(...crates2)
 }
 
-for (const step of steps) {
-  const [number, from, to] = step
-  const crates = d5p2[from - 1].splice(-number)
-  d5p2[to - 1].push(...crates)
-}
-
-d5p1 = d5p1.map((stack) => stack.at(-1)).join('')
-d5p2 = d5p2.map((stack) => stack.at(-1)).join('')
+export const d5p1 = stack1.map((stack) => stack.at(-1)).join('')
+export const d5p2 = stack2.map((stack) => stack.at(-1)).join('')
