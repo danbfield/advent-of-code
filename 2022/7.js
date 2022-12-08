7741fe1a9a4a32840813464ba94b32809a023382
Dan Butterfield <danrbutterfield@gmail.com>
2022-12-08 10:41:24 +0000

    Add day 7 boiler plate

---
+++ 2022/7.js
@@ -0,0 +1,8 @@
+import { parseInputAsStrings } from '../inputs/helper.js'
+
+const input = parseInputAsStrings('inputs/2022/7.txt')
+
+console.log(input)
+
+export const d7p1 = 0
+export const d7p2 = 0
--- server.js
+++ server.js
@@ -5,6 +5,7 @@ import { d3p1, d3p2 } from './2022/3.js'
 import { d4p1, d4p2 } from './2022/4.js'
 import { d5p1, d5p2 } from './2022/5.js'
 import { d6p1, d6p2 } from './2022/6.js'
+import { d7p1, d7p2 } from './2022/7.js'
 
 const app = express()
 const port = 8080
@@ -17,6 +18,7 @@ app.listen(port, () => {
   console.log({ 'day4.0': d4p1, 'day4.5': d4p2 })
   console.log({ 'day5.0': d5p1, 'day5.5': d5p2 })
   console.log({ 'day6.0': d6p1, 'day6.5': d6p2 })
+  console.log({ 'day7.0': d7p1, 'day7.5': d7p2 })
 })
 
 app.get('/', (req, res) => {
