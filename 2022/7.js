991475598f4f6bfa00bd23f3e5a3d1b7a7714fe0
Dan Butterfield <danrbutterfield@gmail.com>
2022-12-09 11:09:28 +0000

    Add day 7 part 2

--- 2022/7.js
+++ 2022/7.js
@@ -23,10 +23,13 @@ for (const command of input) {
   }
 }
 
-console.log(directory)
+const DISK_SPACE = 70000000
+const REQUIRED_SPACE = 30000000
 
 export const d7p1 = Object.values(directory)
   .filter((value) => value < 100000)
   .reduce(sum, 0)
 
-export const d7p2 = 0
+export const d7p2 = Object.values(directory)
+  .filter((value) => value >= REQUIRED_SPACE - (DISK_SPACE - directory['/']))
+  .sort((a, b) => a - b)[0]
