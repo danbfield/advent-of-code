023c4d29c01e5524309b9149b404b17cf373b0a8
Dan Butterfield <danrbutterfield@gmail.com>
2022-12-09 15:57:00 +0000

    Add iteration to get trees to consider

--- 2022/8.js
+++ 2022/8.js
@@ -4,5 +4,14 @@ const input = parseInputAsStrings('inputs/2022/8.txt')
 
 console.log(input)
 
+for (let i = 1; i < input.length - 1; i++) {
+  for (let j = 1; j < input[i].length - 1; j++) {
+    const horizontal = Number(input[i].substring(1, input[i].length - 1))
+    const vertical = Number(input[j].substring(1, input[j].length - 1))
+
+    console.log({ horizontal, vertical })
+  }
+}
+
 export const d8p1 = 0
 export const d8p2 = 0
