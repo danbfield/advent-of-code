4100a70b93da0fda42d5b73ea93c97f28dc2da9c
Dan Butterfield <danrbutterfield@gmail.com>
2022-12-09 10:06:48 +0000

    Add directory values

--- 2022/7.js
+++ 2022/7.js
@@ -2,37 +2,28 @@ import { parseInputAsStrings } from '../inputs/helper.js'
 
 const input = parseInputAsStrings('inputs/2022/7.txt')
 
-// - / (dir)
-//   - a (dir)
-//     - e (dir)
-//       - i (file, size=584)
-//     - f (file, size=29116)
-//     - g (file, size=2557)
-//     - h.lst (file, size=62596)
-//   - b.txt (file, size=14848514)
-//   - c.dat (file, size=8504156)
-//   - d (dir)
-//     - j (file, size=4060174)
-//     - d.log (file, size=8033020)
-//     - d.ext (file, size=5626152)
-//     - k (file, size=7214296)
+const directory = {}
+const paths = []
 
-console.log(input)
+for (const command of input) {
+  if (command.startsWith('$ cd ')) {
+    const current = command.split('$ cd ')[1]
+    command.includes('..') ? paths.pop() : paths.push(current)
+  } else if (/^\d/.test(command)) {
+    const temp = [...paths]
 
-for (let i = 0; i < input.length; i++) {
-  console.log(command)
+    while (temp.length) {
+      const joinedPaths = temp.join('_')
+      const fileSize = Number(command.split(' ')[0])
 
-  const command = input[i].split(' ')
-  const isNumber = new RegExp('^[0-9]+$')
+      directory[joinedPaths] = (directory[joinedPaths] || 0) + fileSize
 
-  if (isNumber.test(command[0])) {
-    console.log('file')
-  }
-
-  if (command[1].includes('cd')) {
-    const dir = command[2]
-    console.log('cd command')
+      temp.pop()
+    }
   }
 }
+
+console.log(directory)
+
 export const d7p1 = 0
 export const d7p2 = 0
