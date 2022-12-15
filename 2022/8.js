b887020f86c27a407c116122cf6732eb0f1b7337
Dan Butterfield <danrbutterfield@gmail.com>
2022-12-15 11:07:24 +0000

    Replace with more readable loop

--- 2022/8.js
+++ 2022/8.js
@@ -1,17 +1,28 @@
-import { parseInputAsStrings } from '../inputs/helper.js'
+import {
+  parseInputAsStrings,
+  deepConvertStringToInt,
+} from '../inputs/helper.js'
 
 const input = parseInputAsStrings('inputs/2022/8.txt')
 
 console.log(input)
 
-for (let i = 1; i < input.length - 1; i++) {
-  for (let j = 1; j < input[i].length - 1; j++) {
-    const horizontal = Number(input[i].substring(1, input[i].length - 1))
-    const vertical = Number(input[j].substring(1, input[j].length - 1))
+// 30373
+// 25512 etc.
+input.forEach((row, rowIndex, rows) => {
+  // 3, 0, 3, 7, 3
+  // 2, 5, 5, 1, 2 etc.
+  console.log({ row })
+  row.split('').forEach((col, colIndex, cols) => {
+    const isEdge =
+      !rowIndex ||
+      rowIndex === rows.length - 1 ||
+      !colIndex ||
+      colIndex === cols.length - 1
 
-    console.log({ horizontal, vertical })
-  }
-}
+    console.log({ col, isEdge })
+  })
+})
 
 export const d8p1 = 0
 export const d8p2 = 0
